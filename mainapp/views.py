e73d676f422e6a98d1296c927c625d9e793dcc3f
michael <kurashevmichael@gmail.com>
2022-03-25 16:39:40 +0300

    dz-2

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
-
+import json
+from geekshop.settings import BASE_DIR
 # Create your views here.
 
 def index(request):
-    return render(request, 'mainapp/index.html')
+    context = {
+        "title": "Geekshop",
+    }
+    return render(request, 'mainapp/index.html', context)
 
 def products(request):
-    return render(request, 'mainapp/products.html')
+    products_json = json.load(open (f'{BASE_DIR}/mainapp/fixtures/products.json', "r"))
+    categories = ["Новинки", "Одежда", "Обувь", "Аксессуары", "Подарки"]
+    context = {
+        "title": "Geekshop - Каталог",
+        "products": products_json['products'],
+        "categories": categories,
+    }
+    return render(request, 'mainapp/products.html', context)
